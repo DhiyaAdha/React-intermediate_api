fddcbefc2504ee7ac86b924f42b27379c358fd27
Dhiya <adiadiadha@google.com>
2023-10-30 06:14:57 +0700

    search dinamic with label

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -5,7 +5,13 @@ import axios from "axios";
 
 class BlogPost extends Component {
   state = {
-    post: []
+    post: [],
+    formBlogPost: {
+      id: 1,
+      title: "",
+      body: "",
+      userId: 1,
+    }
   }
 
   getFecthAPI = () => {
@@ -43,6 +49,21 @@ class BlogPost extends Component {
       });
   }
 
+  handleFormChange = (event) => {
+    // console.log("Form Change", event.target);
+    let formBlogPostNew = { ...this.state.formBlogPost };
+    formBlogPostNew[event.target.value] = event.target.value;
+    let title = event.target.value;
+    this.setState(
+      {
+        formBlogPost: formBlogPostNew,
+      },
+      () => {
+        console.log("value obj formBlogPost", this.state.formBlogPost);
+      }
+    );
+  }
+
   componentDidMount() {
     // fecth API
     // this.getFecthAPI();
@@ -54,11 +75,36 @@ class BlogPost extends Component {
         return (
           <Fragment>
             <p className="section-title">Blog Post</p>
-            {
-              this.state.post.map(post => {
-              return <Post data={post} key={post.id} title={post.title} body={post.author} remove={this.handleRemove} />;
-              })
-            }
+            <div className="form-add-post">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                name="title"
+                placeholder="add title"
+                onChange={this.handleFormChange}
+              />
+              <label htmlFor="body">Blog Content</label>
+              <textarea
+                type="text"
+                name="body"
+                id="body"
+                cols="30"
+                rows="10"
+                placeholder="add blog content"
+                onChange={this.handleFormChange}></textarea>
+              <button className="btn-submit">Simpan</button>
+            </div>
+            {this.state.post.map((post) => {
+              return (
+                <Post
+                  data={post}
+                  key={post.id}
+                  title={post.title}
+                  body={post.author}
+                  remove={this.handleRemove}
+                />
+              );
+            })}
           </Fragment>
         );
             
